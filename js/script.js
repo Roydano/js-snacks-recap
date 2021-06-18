8a37cdaac678e8467036530ccbc148e4137dc5f9
Roydano <roy.dano90@gmail.com>
2021-06-18 17:24:52 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -10,18 +10,45 @@
 
 
 
-//FUNZIONE PER GENERARE I NUMERI RANDOM DA INSERIRE NELL'ARRAY VUOTO
+
+
+const minNum = parseInt(prompt('Inserisci un numero minimo compreso tra 1 e 10'));
+
+const maxNum = parseInt(prompt('Inserisci un numero massimo compreso tra 1 e 10 maggiore del numero precedente'));
+
+
+
+//?FUNZIONE PER GENERARE I NUMERI RANDOM DA INSERIRE NELL'ARRAY VUOTO
 
 const randomNum = (min, max) => Math.floor(Math.random() * max - min + 1) + min;
 
 const numeri = [];
+const rangeNum = [];
 
-for(let i = 0; i < 10; i++){
+for(let i = 0; numeri.length < 10; i++){
     let genNum = randomNum(1, 100);
 
     if(!numeri.includes(genNum)){
         numeri.push(genNum);
-    }
+    };
 }
-
 console.log(numeri);
+
+rangeForPrint(numeri, minNum, maxNum);
+
+console.log(rangeNum);
+
+
+
+//! FUNZIONE
+
+function rangeForPrint(array, min, max){
+
+    array.forEach((element, index) => {
+        console.log(index, element);
+        if(index >= min && index <= max){
+            rangeNum.push(element)
+        }
+    });
+
+}
